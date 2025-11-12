145246396415808c736a6ab5cd7e6be521f0722b
SP-ESH <codewithme20.02@gmail.com>
2025-11-12 16:02:15 +0530

    Added Component folder

---
+++ frontend/src/component/CreateTodo.jsx
@@ -0,0 +1,11 @@
+const CreateTodo = () => {
+    return (
+        <div>
+            <input type="text" placeholder="title" /> <br />
+            <input type="text" placeholder="description" /> <br />
+
+            <button>Add to Todo</button>
+        </div>
+    )
+}
+export default CreateTodo
